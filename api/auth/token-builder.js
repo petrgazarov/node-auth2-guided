e59e29eb3a3de88cbef7e6cf60f8ca16d6cc8c1a
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-20 18:40:15 +0200

    wip

--- api/auth/token-builder.js
+++ api/auth/token-builder.js
@@ -2,5 +2,8 @@ const jwt = require('jsonwebtoken')
 const { jwtSecret } = require('../../config/secrets')
 
 module.exports = function (user) {
-
+  const payload = {
+    subject: user.id,
+    
+  }
 }
