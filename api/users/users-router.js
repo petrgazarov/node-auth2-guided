9063a8814380517122f9338a92a04a1c4aba20b8
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-20 19:42:16 +0200

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,10 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
 
+function onlyAdmin(req, res, next) {
+  const { decodedJwt } = req
+}
+
 router.get("/", restricted, onlyAdmin, (req, res, next) => {
   Users.find()
     .then(users => {
