3516df33f15ce2db7e8826ddf467e86b29f89216
Petr Gazarov <petrgazarov@gmail.com>
2021-08-05 20:17:15 -0700

    Add onlyAdmin check

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,7 +3,20 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
 
-router.get("/", restricted, (req, res, next) => {
+const onlyAdmin = (req, res, next) => {
+  // 1. Read token data from decoded JWT
+  // 2. Check if role is admin. If it is, proceed. If it is not, then terminate the request with a 403.
+
+  const { decodedToken } = req;
+
+  if (decodedToken.role !== 'admin') {
+    next({ status: 403, message: 'Sorry pal, not allowed.' });
+  } else {
+    next();
+  }
+}
+
+router.get("/", restricted, onlyAdmin, (req, res, next) => {
   Users.find()
     .then(users => {
       res.json(users);
